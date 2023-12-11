0370c62672fd523e4d7f38032b5166b6f95e3196
Jirgort <Jirgort@gmail.com>
2023-12-11 00:38:07 -0600

    Pagina nuevo cliente
    Uso de hook navigate para volver a la pagina anterior

--- crm-react/src/pages/NuevoCliente.jsx
+++ crm-react/src/pages/NuevoCliente.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function NuevoCliente() {
+  const navigate=useNavigate()
   return (
-    <div>
-      Cleintes nuevo
-    </div>
-  )
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Clientes</h1>
+      <p className="mt-3">
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+      <div className="flex justify-end">
+        <button className=" bg-blue-800 text-white px-3 py-1 font-bold uppercase" onClick={()=>navigate(-1)}>
+          Volver
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default NuevoCliente
+export default NuevoCliente;
