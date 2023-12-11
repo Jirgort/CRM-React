3095473092a8e6e35c581f792cba8fa1983fc683
Jirgort <Jirgort@gmail.com>
2023-12-11 00:49:32 -0600

    Fomulario :cherries:

--- crm-react/src/pages/NuevoCliente.jsx
+++ crm-react/src/pages/NuevoCliente.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import Formulario from "../components/Formulario";
 function NuevoCliente() {
   const navigate=useNavigate()
   return (
@@ -12,6 +12,14 @@ function NuevoCliente() {
         <button className=" bg-blue-800 text-white px-3 py-1 font-bold uppercase" onClick={()=>navigate(-1)}>
           Volver
         </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+
+        
+        <Formulario/>
+        <input type="submit" className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg" value="Registrar Cliente"/>
+        </form>
       </div>
     </>
   );
