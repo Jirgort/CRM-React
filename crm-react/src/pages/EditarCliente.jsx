a543c256bab05c485651f95fdc4d4328a5ed01c6
Jirgort <Jirgort@gmail.com>
2023-12-15 21:02:05 -0600

    Obteniendo usuario y mostrar error si no lo encuentra

---
+++ crm-react/src/pages/EditarCliente.jsx
@@ -0,0 +1,21 @@
+import {obtenerCliente} from '../data/clientes'
+export async function loader({params}){
+   const cliente= await obtenerCliente(params.clienteId)
+   if(Object.values(cliente).length===0){
+        throw new Response('',{
+            status:404,
+            statusText:'El cliente no fue encontrado'
+        })
+   }
+   return cliente
+}
+function EditarCliente() {
+
+  return (
+    <div>
+      Editar Cliente
+    </div>
+  )
+}
+
+export default EditarCliente
